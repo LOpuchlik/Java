350d74529ea9a1e3f9e20aa2b86501ca385df7a6
Michal <michal.opuchlik@gmail.com>
2020-03-12 20:12:27 +0100

    Added examples with static attribute usage for example of Math.PI with appropriate rounding, static method - Math.pow, and factory methods from NumberFormat class

--- Nauka_programowania/src/Obiekty_i_klasy/statyczne/Rounding.java
+++ Nauka_programowania/src/Obiekty_i_klasy/statyczne/Rounding.java
@@ -1,5 +1,8 @@
 package Obiekty_i_klasy.statyczne;
 
+import java.text.NumberFormat;
+import java.time.LocalDate;
+
 public class Rounding {
     public static void main(String[] args) {
         final double PI = Math.PI;
@@ -11,6 +14,18 @@ public class Rounding {
         double res = Math.pow(x, 10); // metoda statyczna - nie jest potrzebny zaden obiekt klasy Math
         System.out.println("2^10 is equal to: " + (int)(res));
 
+
+        // tworzenie obiektów przy pomcy metod fabrycznych
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
+        double value = 0.12;
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        System.out.println(percent.format(value));
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        System.out.println(currency.format(value));
 
     }
 }
