12f981d8f89c51380fd7db272ab15fddb0fe8e7d
Michal <michal.opuchlik@gmail.com>
2020-03-12 20:02:46 +0100

    Added examples with static and final static for Math.PI with appropriate rounding

--- Nauka_programowania/src/Obiekty_i_klasy/statyczne/Rounding.java
+++ Nauka_programowania/src/Obiekty_i_klasy/statyczne/Rounding.java
@@ -1,11 +1,16 @@
 package Obiekty_i_klasy.statyczne;
 
-
 public class Rounding {
     public static void main(String[] args) {
         final double PI = Math.PI;
         System.out.println(PI);
         System.out.println(Math.round(PI*1000000d)/1000000d);
         System.out.println(Math.round(PI*10000d)/10000d);
+
+        double x = 2;
+        double res = Math.pow(x, 16); // metoda statyczna - nie jest potrzebny zaden obiekt klasy Math
+        System.out.println("2^16 is equal to: " + (int)(res));
+
+
     }
 }
