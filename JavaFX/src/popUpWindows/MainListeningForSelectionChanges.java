18ae0d530f31ea63720e0b6190b6a2bd03e548c0
LOpuchlik <lidia.opuchlik@gmail.com>
2020-03-15 16:39:27 +0100

    Added implementation of Choice Box with listening for selection changes with getSelectionModel().selectedItemProperty() ...

--- JavaFX/src/popUpWindows/MainListeningForSelectionChanges.java
+++ JavaFX/src/popUpWindows/MainListeningForSelectionChanges.java
@@ -20,17 +20,20 @@ public class MainListeningForSelectionChanges extends  Application{
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Choice Box");
-        Button button = new Button("Click");
 
-        ChoiceBox<String> strChoiceBox = new ChoiceBox<>();
-        strChoiceBox.getItems().addAll("Apples", "Bananas", "Lemons", "Strwaberries", "Blueberries");
-        strChoiceBox.setValue("Apples"); // Set a default value
 
-        button.setOnAction(evnt -> getChoice(strChoiceBox));
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("Apples", "Bananas", "Lemons", "Strwaberries", "Blueberries");
+        choiceBox.setValue("Apples"); // Set a default value
+
+        // Listen for selection changes
+        choiceBox.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> System.out.println(newValue));
+
+
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(15,15,15,15));
-        layout.getChildren().addAll(strChoiceBox, button);
+        layout.getChildren().addAll(choiceBox);
 
 
         Scene scene = new Scene(layout, 300, 200);
@@ -38,10 +41,6 @@ public class MainListeningForSelectionChanges extends  Application{
         window.show();
     }
 
-    private void getChoice(ChoiceBox<String> strChoiceBox) {
-        String food = strChoiceBox.getValue();
-        System.out.println(food);
-    }
 }
 
 
